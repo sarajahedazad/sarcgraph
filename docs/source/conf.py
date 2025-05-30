080d7614d9498c9348d65f320bd3dbf393d9d8a3
saeedmhz <saeedmhz@bu.edu>
2025-05-30 17:41:12 -0400

    flake8 error fixed

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,10 +5,9 @@
 
 import os
 import sys
+import sarcgraph
 
 sys.path.insert(0, os.path.abspath("../../"))
-
-import sarcgraph
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
